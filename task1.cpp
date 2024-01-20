ea506f96cbe2096a06839d9fbd23d63055f44002
JoyKarmoker <Joykarmoker353@gmail.com>
2024-01-20 12:00:58 +0600

    MPI Completed

---
+++ task1.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <vector>
+#include "mpi.h"
+
+int main(int argc, char *argv[]) {
+    int m, n, p, k;
+    int number_of_matrix_in_each_processor;
+    int total_number_of_processors, rank;
+    double cal_start_time, cal_end_time;
+
+    int *mat_a;
+    int *mat_b;
+    int *mat_c = nullptr;  // Initialize mat_c to nullptr
+
+    int *l_a;
+    int *l_b;
+    int *l_c;
+
+    int max_matrix_element_value = 50;
+    int min_matrix_element_value = 1;
+    int matrix_element_value = min_matrix_element_value;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm comm = MPI_COMM_WORLD;
+    MPI_Status status;
+    MPI_Comm_size(comm, &total_number_of_processors);
+    MPI_Comm_rank(comm, &rank);
+
+    if (rank == 0) {
+        std::cout << "Give K: ";
+        std::cin >> k;
+        std::cout << "Give M: ";
+        std::cin >> m;
+        std::cout << "Give N: ";
+        std::cin >> n;
+        std::cout << "Give P: ";
+        std::cin >> p;
+
+        mat_a = (int *) malloc(k * m * n * sizeof(int));
+        mat_b = (int *) malloc(k * n * p * sizeof(int));
+        mat_c = (int *) malloc(k * m * p * sizeof(int));
+
+        for (int i = 0; i < m * n * k; i++) {
+            if (matrix_element_value > max_matrix_element_value)
+                matrix_element_value = min_matrix_element_value;
+            mat_a[i] = matrix_element_value++;
+        }
+
+        for (int i = 0; i < n * p * k; i++) {
+            if (matrix_element_value > max_matrix_element_value)
+                matrix_element_value = min_matrix_element_value;
+            mat_b[i] = matrix_element_value++;
+        }
+    }
+    MPI_Bcast(&k, 1, MPI_INT, 0, comm);
+    MPI_Bcast(&m, 1, MPI_INT, 0, comm);
+    MPI_Bcast(&n, 1, MPI_INT, 0, comm);
+    MPI_Bcast(&p, 1, MPI_INT, 0, comm);
+    
+    number_of_matrix_in_each_processor = k / total_number_of_processors;
+    l_a = (int *) malloc(number_of_matrix_in_each_processor * m * n * sizeof(int));
+    l_b = (int *) malloc(number_of_matrix_in_each_processor * n * p * sizeof(int));
+    l_c = (int *) malloc(number_of_matrix_in_each_processor * m * p * sizeof(int));
+    MPI_Scatter(mat_a, number_of_matrix_in_each_processor * m * n, MPI_INT,
+                l_a, number_of_matrix_in_each_processor * m * n, MPI_INT, 0, comm);
+    MPI_Scatter(mat_b, number_of_matrix_in_each_processor * n * p, MPI_INT,
+                l_b, number_of_matrix_in_each_processor * n * p, MPI_INT, 0, comm);
+    // MPI_Barrier(comm);
+
+    int m_a[number_of_matrix_in_each_processor][m][n];
+    int m_b[number_of_matrix_in_each_processor][n][p];
+    int m_c[number_of_matrix_in_each_processor][m][p];
+
+    int idx = 0;
+    for (int c = 0; c < number_of_matrix_in_each_processor; c++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                m_a[c][i][j] = l_a[idx++];
+            }
+        }
+    }
+
+    idx = 0;
+    for (int c = 0; c < number_of_matrix_in_each_processor; c++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < p; j++) {
+                m_b[c][i][j] = l_b[idx++];
+            }
+        }
+    }
+
+    cal_start_time = MPI_Wtime();
+    for (int c = 0; c < number_of_matrix_in_each_processor; c++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < p; j++) {
+                m_c[c][i][j] = 0;
+
+                for (int z = 0; z < n; z++) {
+                    m_c[c][i][j] += m_a[c][i][z] * m_b[c][z][j];
+                }
+            }
+        }
+    }
+
+    cal_end_time = MPI_Wtime();
+    printf("PID %d: Total time: %lf\n", rank, cal_end_time - cal_start_time);
+    idx = 0;
+    for (int c = 0; c < number_of_matrix_in_each_processor; c++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                l_c[idx++] = m_c[c][i][j];
+            }
+        }
+    }
+
+    MPI_Gather(l_c, number_of_matrix_in_each_processor * m * p, MPI_INT, mat_c, number_of_matrix_in_each_processor * m * p, MPI_INT, 0, comm);
+
+    if (rank == 0) {
+        // Display results if needed
+    }
+
+
+    MPI_Finalize();
+    free(l_a);
+    free(l_b);
+    free(l_c);
+    if (rank == 0) {
+        free(mat_a);
+        free(mat_b);
+        free(mat_c);
+    }
+    return 0;
+}
---
+++ task2.cpp
@@ -0,0 +1,169 @@
+//... To run: mpirun -n 4 ./phonebook-search phonebook1.txt phonebook1.txt
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include<vector>
+#include <mpi.h>
+#include <algorithm>  // for std::transform
+#include <cctype>
+
+
+using namespace std;
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    const int max_name_length = 100;
+    int found_number = 0;
+    char search_name[max_name_length];
+    bool stopSearch = false;
+    bool printed = false;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // This part checks if at least two file names are given
+    if (argc < 2) {
+        if (rank == 0) {
+            std::cerr << "Usage: mpiexec -n <number_of_processes> ./your_executable file1.txt file2.txt ...\n";
+        }
+        MPI_Finalize();
+        return 1;
+    }
+
+    vector<int> lines_per_file(argc, 0);
+
+    // Print file names in rank 0
+    if (rank == 0) 
+    {
+        cout << "Give the name you want to search: ";
+        cin.getline(search_name, max_name_length);
+        // Convert the search_name to uppercase
+        transform(search_name, search_name + strlen(search_name), search_name, ::toupper);
+        // std::cout << "Rank 0: Received file names:\n";
+        for (int i = 1; i < argc; ++i) {
+            // std::cout << argv[i] << "\n";
+
+            // Open the file
+            std::ifstream file(argv[i]);
+            if (file.is_open()) {
+                std::string line;
+                int lineCount = 0;
+
+                // Count lines in the file
+                while (std::getline(file, line)) {
+                    ++lineCount;
+                }
+
+                //std::cout << "Number of lines in " << argv[i] << ": " << lineCount << "\n";
+                file.close();
+                lines_per_file[i] = lineCount;
+            } else {
+                std::cerr << "Unable to open file: " << argv[i] << "\n";
+            }
+        }
+    }
+    
+    // Broadcast linesPerFile to all processes
+    MPI_Bcast(lines_per_file.data(), lines_per_file.size(), MPI_INT, 0, MPI_COMM_WORLD);
+    // Broadcast the search name from Process 0 to all other processes
+    MPI_Bcast(search_name, max_name_length, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    for(int i=1; i<argc; i++)
+    {
+       if(stopSearch) //if found in other process
+            break;
+        ifstream file(argv[i]);
+        if(file.is_open())
+        {
+            string line;
+            int linesToRead = lines_per_file[i] / size; // Calculate lines per process
+            int startLine = rank * linesToRead;
+            if (rank == size - 1) {
+                linesToRead += lines_per_file[i] % size;
+            }
+        
+            /*std::cout << " rank: " <<rank << " lines to read in " << argv[i] << ": " << linesToRead
+            << " starting point is: " << startLine << "\n";*/
+
+            for (int j = 0; j < startLine; ++j) 
+            {
+                getline(file, line); // Skip lines
+            }
+
+            for (int j = 0; j < linesToRead; ++j) {
+                if(stopSearch) //if found in other process
+                    break;
+                getline(file, line);
+                // Process the line
+                istringstream iss(line);
+                string name;
+                int number;
+
+                // Extract name and number from the string stream
+                iss >> name >> number;
+
+                // Check if extraction was successful
+                if (iss) {
+                    //std::cout << "Rank " << rank << ": Name: " << name << ", Number: " << number << "\n";
+                    // Process the name and number as needed
+                    transform(name.begin(), name.end(), name.begin(), ::toupper);
+                    if(name == search_name)
+                    {
+                        cout << name << " : " << number << " Found in rank: " << rank  << " file: " << argv[i] <<endl;
+                        found_number = number;
+                        stopSearch = true;
+                        break;
+                    }
+                } else {
+                    std::cerr << "Failed to extract name and number from the line.\n";
+                }
+
+            }
+            file.close();
+        }
+        else
+        {
+            std::cerr << "Unable to open file: " << argv[i] << "in Rank: " << rank <<"\n";
+        }
+    }
+
+    // MPI_Barrier(MPI_COMM_WORLD);
+    if(rank != 0)
+    {
+        MPI_Send(&found_number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+
+    if(rank == 0)
+    {
+        int actual_number_found = 0;
+        if(found_number != 0)
+        {
+            actual_number_found = found_number;
+        }
+        
+        for (int i=1; i<size; i++)
+        {
+            MPI_Recv(&found_number, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if(found_number !=0 )
+            {
+                actual_number_found = found_number;
+            }
+        }
+        
+        
+        
+        // Convert the search_name to lowerrcase
+        transform(search_name, search_name + strlen(search_name), search_name, ::tolower);
+        search_name[0] = toupper(search_name[0]);
+        if(actual_number_found != 0)
+             cout << search_name << " : " << actual_number_found <<endl;
+        else
+            cout << search_name << " : Not found" <<endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
